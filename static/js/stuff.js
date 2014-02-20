7d0619aa02c24a13ecc5590ac98332ec0714a613
Jessica Hao <swordyfishie@gmail.com>
2014-02-19 20:50:24 -0800

    Completed Hello World problem!

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -7,18 +7,28 @@ $('#say_hello').click(function() {alert('Hello world!')
 
 
 // Problem 2 (Houdini) ------------------------------------------------------
-$('#disappear').click(function() {
+$('#disappear').click(function() { 
+	$('#houdini_text').hide();
   //WRITE CODE HERE
 });
 
 $('#reappear').click(function() {
+	$('#houdini_text').show();
   //WRITE CODE HERE
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 // WRITE CODE HERE
-
+$('#pinkie').click(function(){
+	$('#tickled_text').css('color','pink');
+});
 
 // Problem 4 (Greet Me) -----------------------------------------------------
+$('#my_name_button').click(function(){
+	alert('hi '+$('#my_name').val());
+
+});
+
+
 // WRITE CODE HERE

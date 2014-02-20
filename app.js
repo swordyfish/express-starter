64be389058c0429a5e358d1c23eeacafdfc86fee
Jessica Hao <swordyfishie@gmail.com>
2014-02-19 19:26:07 -0800

    Completed Hello World problem!

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.get('/calc', function(req, res) {
 	res.render('calc.html', { });
 });
 
+app.get('/stuff', function(req, res) {
+	res.render('stuff.html', { });
+});
+
 ///////////////////////////////////////////////////////////////////////////////
 // RUN CONFIGURATION                                                         //
 ///////////////////////////////////////////////////////////////////////////////
---
+++ static/js/stuff.js
@@ -0,0 +1,24 @@
+alert('Poopiehead!!'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function() {alert('Hello world!')
+  // WRITE CODE HERE
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE

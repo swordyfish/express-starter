1bf1e1c2ab48aa56db251c17fbd565918b88a07c
Jessica Hao <swordyfishie@gmail.com>
2014-03-05 19:02:30 -0800

    Finished Lines and Curves!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -8,11 +8,32 @@ $(document).ready(function() {
   });
 
   $('#p1').click(function(){
-  	context.strokeRect(100,100,200,100);
+  	context.strokeRect(250,100,200,100);
   });
 
   $('#p2').click(function(){
   	context.strokeRect(100,100,100,100);
+  });
+
+  $('#p3').click(function(){ 
+  	context.beginPath()
+  	context.arc(100,150,100,Math.PI/6,Math.PI/2);
+  	context.stroke()
+  });
+
+    $('#p4').click(function(){ 
+  	context.beginPath()
+  	context.arc(100,100,100,Math.PI/2,5*(Math.PI/2));
+  	context.closePath()
+  	context.stroke()
+  });
+
+    $('#p5').click(function(){ 
+  	context.beginPath()
+  	context.moveTo(100,100);
+  	context.lineTo(200,200);
+  	context.closePath()
+  	context.stroke()
   });
 
   //---------------------------------------------------------------------------

c16a6d62ee5bc01c8ac0b1d6124160f980f19926
Jessica Hao <swordyfishie@gmail.com>
2014-03-05 18:40:13 -0800

    Mastered right angles!

---
+++ static/js/canvas.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  //this is how we acquire control of the canvas
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
+
+  $('#clear').click(function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  $('#p1').click(function(){
+  	context.strokeRect(100,100,200,100);
+  });
+
+  $('#p2').click(function(){
+  	context.strokeRect(100,100,100,100);
+  });
+
+  //---------------------------------------------------------------------------
+  //Write your code for p1-p12 here
+  //
+
+});

9b6928bcc32a423bf31568b3f0f745335386bf08
Jessica Hao <swordyfishie@gmail.com>
2014-03-12 19:33:13 -0700

    Finished Triplets!

--- static/js/funcs.js
+++ static/js/funcs.js
@@ -20,6 +20,23 @@ $(document).ready(function() {
     context.stroke();
   };
 
+var drawTriplet = function(x, y, radius, color) {
+    context.strokeStyle=color
+    context.beginPath()
+    context.arc(x,y-radius*(1/3),radius,Math.PI/2,5*(Math.PI/2))
+    context.closePath()
+    context.stroke()
+
+    context.beginPath()
+    context.arc(x-radius*(2/3),y+radius*(2/3),radius,Math.PI/2,5*(Math.PI/2))
+    context.closePath()
+    context.stroke()
+
+    context.beginPath()
+    context.arc(x+radius*(2/3),y+radius*(2/3),radius,Math.PI/2,5*(Math.PI/2))
+    context.closePath()
+    context.stroke()
+};
   // Write drawTriplet function here
 
   // Challenge:
@@ -49,6 +66,21 @@ $(document).ready(function() {
     drawCircle(325,325,25, 'green');
     drawSquare(275,300,50, 'red');
     drawCircle(300,325,25, 'blue');
+  });
+
+  $('#p6').click(function(){
+    drawTriplet(300,250,50, 'green');
+  });
+
+  $('#p7').click(function(){
+    drawTriplet(400,250,50, 'blue');
+  });
+
+   $('#p8').click(function(){
+    drawTriplet(100,100,50, 'purple');
+    drawTriplet(400,100,50, 'teal');
+    drawTriplet(100,300,50, 'olive');
+    drawTriplet(400,300,50, 'maroon');
   });
 
   //---------------------------------------------------------------------------
